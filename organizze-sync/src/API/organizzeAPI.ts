d09bbc898bb56613b6440ea85129b67f449602d4
Felipe Leão <felipe@felipeleao.com>
2025-03-06 18:25:05 -0300

    feat: create transactions by year

--- organizze-sync/src/API/organizzeAPI.ts
+++ organizze-sync/src/API/organizzeAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const CATEGORY_URL:string = `${process.env.ORGANIZZE_API_URL}/categories`
-const TRANSACTIONS_URL:string = `${process.env.ORGANIZZE_API_URL}/transactions`
+const CATEGORY_URL: string = `${process.env.ORGANIZZE_API_URL}/categories`
+const TRANSACTIONS_URL: string = `${process.env.ORGANIZZE_API_URL}/transactions`
 
 function fetchOrganizze(url: string) {
   return axios({
@@ -17,5 +17,29 @@ function fetchOrganizze(url: string) {
 
 export async function getCategories() {
   const response = await fetchOrganizze(CATEGORY_URL)
-  console.log(response.data)
+  return response.data
+}
+
+export async function getTransactions(startDate: string) {
+  const TRANSACTIONS_URL_WITH_PARAMS = `${TRANSACTIONS_URL}?start_date=${startDate}`
+  const response = await fetchOrganizze(TRANSACTIONS_URL_WITH_PARAMS)
+  return response.data
+}
+
+function generateYearTransactionsFn(year: string): Function[] {
+  let months = Array.from({ length: 12 }, (_, value) =>
+    (value + 1).toString().padStart(2, '0')
+  )
+  return months.map((month) => () => getTransactions(`${year}-${month}-01`))
+}
+
+export async function getYearlyTransactions(year: string) {
+  const data: { [key: string]: object[] } = {}
+  const fns = generateYearTransactionsFn(year)
+  const response = await Promise.all([...fns.map((fn) => fn())])
+  response.forEach((value, index) => {
+    const title = (index + 1).toString().padStart(2, '0')
+    data[title] = value
+  })
+  return data
 }
--- organizze-sync/src/index.ts
+++ organizze-sync/src/index.ts
@@ -1,5 +1,37 @@
 import 'dotenv/config'
+import app from './models/App'
 
-import { getCategories } from './API/organizzeAPI'
+app.setTransactions({ year: '2025' }).then(() => {
+  console.log(app.TRANSACTIONS)
+})
+// import { getCategories, getTransactions } from './API/organizzeAPI'
 
-getCategories()
+// const YEAR = '2025'
+// const MONTH = '03'
+
+// const startDate = `${YEAR}-${MONTH}-01`
+
+// async function populateCategories() {
+//   const CATEGORIES: { [prop: number]: string } = {}
+//   const categories: [{ id: number; name: string }] = await getCategories()
+//   categories.map((category) => {
+//     CATEGORIES[category.id] = category.name
+//   })
+//   return CATEGORIES
+// }
+
+// getTransactions(startDate).then((transactions: []) => {
+//   const CATEGORIES = populateCategories()
+//   const transactionsByCategory: {
+//     [prop: number]: { name: string; total: number }
+//   } = {}
+
+//   transactions.map((transaction: { category_id: number }) => {
+//     if (!transactionsByCategory[transaction.category_id]) {
+//       transactionsByCategory[transaction.category_id] = {
+//         name: CATEGORIES[transaction.category_id],
+//         total: 0
+//       }
+//     }
+//   })
+// })
