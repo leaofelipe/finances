dcec325448c95fb67542290c6a301c6aa3976108
Felipe Leão <felipe@felipeleao.com>
2025-05-21 19:32:34 -0300

    feat(summary): create summary by category

--- packages/sync/src/models/Summary.js
+++ packages/sync/src/models/Summary.js
@@ -1,5 +1,7 @@
 const Transactions = require('./Transactions')
 const { MONTHS } = require('../utils/generateMonths')
+const FirebaseService = require('../services/Firebase')
+const COLLECTION = 'summary'
 
 class Summary {
   constructor() {
@@ -11,8 +13,11 @@ class Summary {
     this.summary[year][month] = this.summary[year][month] || {
       tags: {},
       categories: {},
-      credit_cards: {},
-      total: {}
+      total: {
+        credit_cards: 0,
+        income: 0,
+        outcome: 0
+      }
     }
     return this.summary[year][month]
   }
@@ -24,9 +29,18 @@ class Summary {
     })
   }
 
+  createCategorySummary(context, transaction) {
+    const categoryId = transaction.category_id
+    context.categories[categoryId] = context.categories[categoryId] || 0
+    context.categories[transaction.category_id] += transaction.amount_cents
+  }
+
+  createTotalSummary(context, transaction) {}
+
   parseTransactions(context, transactions) {
     transactions.forEach((transaction) => {
       this.createTagSummary(context, transaction)
+      this.createCategorySummary(context, transaction)
     })
     console.log('Summary.parseTransactions', this.summary)
     // this.createTagSummary(data)
@@ -42,13 +56,20 @@ class Summary {
       const context = this.createContext({ year, month })
       this.parseTransactions(context, transactions)
     })
-    return {}
   }
 
   async saveToDatabase({ year = 2025 } = {}) {
-    await this.getYearlyTransactions(year)
-    console.log('Summary.getSummary', year)
-    return {}
+    try {
+      await this.getYearlyTransactions(year)
+      const summaryJSON = JSON.parse(JSON.stringify(this.summary[year]))
+      const response = await FirebaseService.createDocument(COLLECTION, {
+        id: year,
+        ...summaryJSON
+      })
+      return response
+    } catch (error) {
+      throw new Error('Failed to save categories to database')
+    }
   }
 }
 
